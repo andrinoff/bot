8d03c3060db52917e4093b5968f66d2695a5c95d
drew <realandrinoff@gmail.com>
2025-06-13 21:02:07 +0400

    Now, verifies user

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -23,13 +25,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	b.Start(ctx)
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID: update.Message.Chat.ID,
-		Text:   update.Message.Text,
-	})
+	chatID, err := strconv.Atoi(os.Getenv("CHAT_ID"))
+	if err != nil {
+		fmt.Println("Invalid CHAT_ID:", err)
+		return
+	}
+	if int(update.Message.Chat.ID) == chatID {
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   update.Message.Text,
+		})
+	} else {
+		fmt.Println(update.Message.Text, "user not verified")
+	}
+
 }
